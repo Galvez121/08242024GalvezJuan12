931f0a9c534da3bd535bc8fe99d710d4cd70c738
Juan Galvez <55637706+Galvez121@users.noreply.github.com>
2024-08-24 13:48:09 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+      // Module 1 Exercise 1.2
+        string songLyrics = "Oh, well imagine / \n As I\'m pacing the pews in a church corridor / \n And I can\'t help but to hear / \n No, I can\'t help but to hear an exchanging of words...";
+
+        Console.WriteLine(songLyrics);
+    }
+}
