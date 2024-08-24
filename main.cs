5709977eb97c002075fb433ff61f08dd44c8470f
Juan Galvez <55637706+Galvez121@users.noreply.github.com>
2024-08-24 14:18:37 +0000

    Fixed space between the string

--- main.cs
+++ main.cs
@@ -5,13 +5,13 @@ class Program
     public static void Main(string[] args)
     {
         // Module 1 Exercise 1.2
-        string songLyrics1 = "Oh, well imagine / \n As I\'m pacing the pews in a church corridor / \n And I can\'t help but to hear / \n No, I can\'t help but to hear an exchanging of words...";
+        string songLyrics1 = "Oh, well imagine /\nAs I\'m pacing the pews in a church corridor /\nAnd I can\'t help but to hear /\nNo, I can\'t help but to hear an exchanging of words...";
 
-        string songLyrics2 = "Oh-oh-oh-oh-oh, \n oh-oh-oh-oh, \n oh-oh-oh / \n Caught in a bad romance...";
+        string songLyrics2 = "Oh-oh-oh-oh-oh,\noh-oh-oh-oh,\noh-oh-oh /\nCaught in a bad romance...";
 
-        string songLyrics3 = "Coming out of my cage \n And I\'ve been doing just fine \n Gotta gotta be down \n  Because I want it all ";
+        string songLyrics3 = "Coming out of my cage\nAnd I\'ve been doing just fine\nGotta gotta be down\nBecause I want it all ";
 
-        string songLyrics4 = "Now, this is a story all about how \n My life got flipped-turned upside down \n And I\'d like to take a minute \n Just sit right there \n  I\'ll tell you how I became the prince of a town called Bel-Air";
+        string songLyrics4 = "Now, this is a story all about how \nMy life got flipped-turned upside down\nAnd I\'d like to take a minute\nJust sit right there\nI\'ll tell you how I became the prince of a town called Bel-Air";
 
         Console.WriteLine(songLyrics1 + " \n");
         Console.WriteLine(songLyrics2 + " \n");
