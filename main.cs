2d018c38071f5773d6d228d1cd9d00acfb806cdd
Juan Galvez <55637706+Galvez121@users.noreply.github.com>
2024-08-24 14:05:29 +0000

    More songs added

--- main.cs
+++ main.cs
@@ -4,9 +4,19 @@ class Program
 {
     public static void Main(string[] args)
     {
-      // Module 1 Exercise 1.2
-        string songLyrics = "Oh, well imagine / \n As I\'m pacing the pews in a church corridor / \n And I can\'t help but to hear / \n No, I can\'t help but to hear an exchanging of words...";
+        // Module 1 Exercise 1.2
+        string songLyrics1 = "Oh, well imagine / \n As I\'m pacing the pews in a church corridor / \n And I can\'t help but to hear / \n No, I can\'t help but to hear an exchanging of words...";
 
-        Console.WriteLine(songLyrics);
+        string songLyrics2 = "Oh-oh-oh-oh-oh, \n oh-oh-oh-oh, \n oh-oh-oh / \n Caught in a bad romance...";
+
+        string songLyrics3 = "Coming out of my cage \n And I\'ve been doing just fine \n Gotta gotta be down \n  Because I want it all ";
+
+        string songLyrics4 = "Now, this is a story all about how \n My life got flipped-turned upside down \n And I\'d like to take a minute \n Just sit right there \n  I\'ll tell you how I became the prince of a town called Bel-Air";
+
+        Console.WriteLine(songLyrics1 + " \n");
+        Console.WriteLine(songLyrics2 + " \n");
+        Console.WriteLine(songLyrics3 + " \n");
+        Console.WriteLine(songLyrics4);
+
     }
 }
